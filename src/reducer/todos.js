816781a3d2e10cfab784ccdc1656e1319d308d4f
張華容 <chloe012125@gmail.com>
2022-04-20 20:27:35 +0800

    “編輯功能”

--- src/reducer/todos.js
+++ src/reducer/todos.js
@@ -23,14 +23,18 @@ export default function todos(state = initialTasks, action) {
                 ...state.slice(0,action.idx),
                 ...state.slice(action.idx + 1)
             ];
-        // case types.EDIT_TASK:
-        //     const editedTaskList = initialTasks.map (task =>{
-        //         if (id === task.id){
-        //             return {...state, taskName: newState}
-        //         }
-        //         return editedTaskList;
-        //     });
-        //     break;
+        case types.EDIT_TASK:
+            function editTask(id, newName) {
+                const editedTaskList = state.map(task => {
+                  if (id === state.id) {
+                    return {...state, name: newName}
+                  }
+                  return state;
+                });
+                setTasks(editedTaskList);
+              }
+              break;
+        
         case types.TOGGLE_TASK:
             let newState = [...state];
             newState[action.idx].isCompleted = !newState[action.idx].isCompleted;
